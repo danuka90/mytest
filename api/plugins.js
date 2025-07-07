393218b530d72de947c476e403ed7f6610d436e6
Chathuranga <danukadanuka21865@gmail.com>
2025-07-08 00:07:53 +0530

    Update plugins.js

--- api/plugins.js
+++ api/plugins.js
@@ -1,15 +1,3 @@
-import fs from 'fs';
-import path from 'path';
-
 export default function handler(req, res) {
-  const pluginsDir = path.join(process.cwd(), 'test');
-  const files = fs.readdirSync(pluginsDir);
-  const plugins = {};
-
-  for (const file of files) {
-    const filePath = path.join(pluginsDir, file);
-    plugins[file] = fs.readFileSync(filePath, 'utf8');
-  }
-
-  res.status(200).json(plugins);
+  res.status(200).json({ success: true, message: "API works!" });
 }
